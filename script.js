b4463e424a519f26b5b8782c986565bab5634502
Mackie-s <masaki.shimadu@gmail.com>
2022-01-20 06:34:19 +0900

    c-background-coverをc-coverに変更

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 jQuery(function($){
   $(".p-header__menu").on("click", function(){
-    $(".c-background-cover").toggleClass("active");
+    $(".c-cover").toggleClass("active");
     $(".p-sidebar").toggleClass("active");
     $("body").css("position","fixed");
   });
   $(".c-close").on("click", function(){
-    $(".c-background-cover").toggleClass("active");
+    $(".c-cover").toggleClass("active");
     $(".p-sidebar").toggleClass("active");
     $("body").css("position","");
   });
@@ -15,11 +15,11 @@ jQuery(function($){
     
     if($window > bpt){
       $(".p-sidebar").show();
-      $(".c-background-cover").removeClass("active");
+      $(".c-cover").removeClass("active");
       $("body").css("position","");
     }else{
       $(".p-sidebar").removeClass("active");
-      $(".c-background-cover").removeClass("active");
+      $(".c-cover").removeClass("active");
       $("body").css("position","");
     }
   });
